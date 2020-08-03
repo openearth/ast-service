b645db30ff248cb5c994b28b8f5859c6d128182e
micha <micha@0ff102cf-ee28-4064-adf9-66d38ca607b7>
2020-08-03 12:34:43 +0000

    Add geoserver_utils
    git-svn-id: https://svn.oss.deltares.nl/repos/openearthtools/trunk/python/applications/AST@16501 0ff102cf-ee28-4064-adf9-66d38ca607b7

---
+++ ast_python/geoserver_utils.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import os
+from geoserver.catalog import Catalog
+
+# Upload raster file to GeoServer
+
+def geoserver_upload_gtif(layername, resturl, user, password, gtifpath, sld_style = 'PET potential', workspace='TEMP'):
+    
+    # Connect and get workspace
+    cat = Catalog(resturl, username=user, password=password)
+    ws = cat.get_workspace(workspace)
+
+    # Create store
+    print ('Uploading {} at geoserver'.format(layername))
+    ft = cat.create_coveragestore(layername, workspace=ws, data=gtifpath)
+
+    # Associate SLD styling to it
+    layer = cat.get_layer(layername)
+    layer.default_style = sld_style
+    cat.save(layer)
+
+    # Return wms url
+    wmslay = workspace+':'+layername    
+    return wmslay
+
+# Cleanup temporary layers and stores
+def cleanup_temp(cf, workspace='TEMP'):
+
+    # Connect and get workspace
+    cat = Catalog(cf.get('GeoServer', 'rest_url'), username=cf.get('GeoServer', 'user'), password=cf.get('GeoServer', 'pass'))
+    
+    # Layers
+    layers = cat.get_layers()
+    for l in layers:
+        if (workspace+':') in l.name:
+            print('Deleting layer = {}'.format(l.name))
+            try:
+                cat.delete(l)
+                print('OK')
+            except:
+                print('ERR')
+    cat.reload()
+    
+    # Stores
+    stores = cat.get_stores()        
+    print('-------------------')
+    for s in stores:
+        if workspace in s.workspace.name:
+            print('Deleting store = {}'.format(s.name))
+            try:
+                cat.delete(s)
+                print('OK')
+            except:
+                print('ERR')
+    cat.reload()
