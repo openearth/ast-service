ab4ddbf4c93f8c6e9989c672c979a674e8ea87f6
visser_mn <visser_mn@0ff102cf-ee28-4064-adf9-66d38ca607b7>
2020-10-30 00:24:39 +0000

    read new ast_pluvflood_param.json
    git-svn-id: https://svn.oss.deltares.nl/repos/openearthtools/trunk/python/applications/AST@16743 0ff102cf-ee28-4064-adf9-66d38ca607b7

--- ast_python/ast_pluvflood.py
+++ ast_python/ast_pluvflood.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
-from os.path import join, dirname, realpath
 import json
+import math
+from os.path import dirname, join, realpath
+
 from ast_python.ast_utils import *
+
 
 def pluvflood_dict(d):
     return pluvflood(**d)
 
+
 def pluvflood2_dict(d):
     return pluvflood2(**d)
+
 
 def pluvflood_param_dict(d):
     return pluvflood_param(**d)
@@ -18,10 +23,22 @@ def pluvflood_json(jsonstr):
     return pluvflood(**d)
 
 
+def pluvflood2_json(jsonstr):
+    d = json.loads(jsonstr)
+    return pluvflood2(**d)
+
+
+def pluvflood_param_json(jsonstr):
+    d = json.loads(jsonstr)
+    return pluvflood_param(**d)
+
+
 def pluvflood(id, projectArea, area, depth, inflow, returnTime, scenarioName):
     # Data file
-    records_file = join(dirname(dirname(realpath(__file__))), 'tables/' +
-                        scenarioName+'/ast_measures_pluvflood.json')
+    records_file = join(
+        dirname(dirname(realpath(__file__))),
+        "tables/" + scenarioName + "/ast_measures_pluvflood.json",
+    )
     record = find_record(id, records_file)
     # check for too small inflow areas
     if inflow <= 0.01:
@@ -38,7 +55,7 @@ def pluvflood(id, projectArea, area, depth, inflow, returnTime, scenarioName):
         40.0,
         50.0,
         100.0,
-        1.00E+12,
+        1.00e12,
     ]
 
     for i in range(len(effective_depth_list)):
@@ -57,37 +74,37 @@ def pluvflood(id, projectArea, area, depth, inflow, returnTime, scenarioName):
         effective_depth_mm - effective_depth_a
     ) / (effective_depth_b - effective_depth_a)
     return_time_inflow = returnTime * multiplication_factor
-    return_time_projectArea = (return_time_inflow * inflow / (projectArea) +
-                               returnTime * (projectArea - inflow) / projectArea) - returnTime
+    return_time_projectArea = (
+        return_time_inflow * inflow / (projectArea)
+        + returnTime * (projectArea - inflow) / projectArea
+    ) - returnTime
 
     # API needs key/value pairs
-    ret = {
-        "returnTime": return_time_projectArea
-    }
+    ret = {"returnTime": return_time_projectArea}
     return ret
+
 
 def pluvflood2(id, projectArea, area, depth, inflow, returnTime, scenarioName):
     # Data file
-    records_file = join(dirname(dirname(realpath(__file__))), 'tables/' +
-                        scenarioName+'/ast_measures_pluvflood.json')
+    records_file = join(
+        dirname(dirname(realpath(__file__))),
+        "tables/" + scenarioName + "/ast_measures_pluvflood.json",
+    )
     record = find_record(id, records_file)
+    params_file = join(
+        dirname(dirname(realpath(__file__))),
+        "tables/" + scenarioName + "/ast_pluvflood_param.json",
+    )
+    with open(params_file) as f:
+        params = json.load(f)
+
     # check for too small inflow areas
     if inflow <= 0.01:
         inflow = 0.01
     storage_capacity = area * depth
     effective_depth = storage_capacity / inflow  # [m]
     effective_depth_mm = effective_depth * 1000.0
-    effective_depth_list = [
-        0.0,
-        5.0,
-        10.0,
-        20.0,
-        30.0,
-        40.0,
-        50.0,
-        100.0,
-        1.00E+12,
-    ]
+    effective_depth_list = params["effective_depths"]
 
     for i in range(len(effective_depth_list)):
         if effective_depth_list[i] <= effective_depth_mm:
@@ -104,29 +121,29 @@ def pluvflood2(id, projectArea, area, depth, inflow, returnTime, scenarioName):
     multiplication_factor = recurrence_a + (recurrence_b - recurrence_a) * (
         effective_depth_mm - effective_depth_a
     ) / (effective_depth_b - effective_depth_a)
-    return_time_inflow = returnTime * multiplication_factor
-    return_time_projectArea = (return_time_inflow * inflow / (projectArea) +
-                               returnTime * (projectArea - inflow) / projectArea) - returnTime
-    #Perc_RA = 50
-    #A_p_perc = 10
-    #A_p =  A_p_perc * projectArea
-    #F_meas = ( A_p * 2.718281**(multiplication_factor*area) / A_p + Perc_RA /100 * (projectArea – A_p) ) /( A_p + Perc_RA / 100 (projectArea – A_P))
-
+    Fmeas_area = inflow * math.log(multiplication_factor)
 
     # API needs key/value pairs
     ret = {
-        "Fmeas_area": return_time_projectArea,
-        "Perc_RA": 50.0,
-        "A_p_perc": 50.0
-        }
+        "Fmeas_area": Fmeas_area,
+    }
     return ret
-    
+
+
 def pluvflood_param(id, projectArea, area, depth, inflow, returnTime, scenarioName):
     # Data file
+    params_file = join(
+        dirname(dirname(realpath(__file__))),
+        "tables/" + scenarioName + "/ast_pluvflood_param.json",
+    )
+    with open(params_file) as f:
+        params = json.load(f)
+
+    area_paved = projectArea * params["perc_paved"]
 
     # API needs key/value pairs
     ret = {
-        "Perc_RA": 50.0,
-        "A_p_perc": 50.0
-        }
+        "Perc_RA": params["perc_RA"],
+        "A_p": area_paved,
+    }
     return ret
